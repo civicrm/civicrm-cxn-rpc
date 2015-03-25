9d892963937bc8e70f51ec3a1afe59fddf195a63
Tim Otten <totten@civicrm.org>
2015-03-24 20:07:56 -0700

    CRM-16173 - RegistrationClient - Hexadecimal cxn IDs

--- src/RegistrationClient.php
+++ src/RegistrationClient.php
@@ -58,7 +58,7 @@ class RegistrationClient {
     $cxn = $this->cxnStore->getByAppId($appMeta['appId']);
     if (!$cxn) {
       $cxn = array(
-        'cxnId' => base64_encode(crypt_random_string(Constants::CXN_ID_CHARS)),
+        'cxnId' => bin2hex(crypt_random_string(Constants::CXN_ID_CHARS)),
         'secret' => base64_encode(crypt_random_string(Constants::AES_CHARS)),
         'appId' => $appMeta['appId'],
       );
