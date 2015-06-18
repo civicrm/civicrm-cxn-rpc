d7a7aa3d28cdde6398f543feb13c577f667b79ad
Tim Otten <totten@civicrm.org>
2015-06-18 13:33:58 -0700

    Comments - AppStoreInterface, CxnStoreInterface

--- src/AppStore/AppStoreInterface.php
+++ src/AppStore/AppStoreInterface.php
@@ -1,14 +1,39 @@
 <?php
 namespace Civi\Cxn\Rpc\AppStore;
 
+use Civi\Cxn\Rpc\AppMeta;
+
 interface AppStoreInterface {
 
+  /**
+   * @return array
+   *   List of App IDs.
+   */
   public function getAppIds();
 
+  /**
+   * @param string $appId
+   *   The application's globally unique ID.
+   * @return array
+   *   The application metadata.
+   * @see AppMeta
+   */
   public function getAppMeta($appId);
 
+  /**
+   * @param string $appId
+   *   The application's globally unique ID.
+   * @return string
+   *   PEM-encoded.
+   */
   public function getPublicKey($appId);
 
+  /**
+   * @param string $appId
+   *   The application's globally unique ID.
+   * @return string
+   *   PEM-encoded.
+   */
   public function getPrivateKey($appId);
 
 }
